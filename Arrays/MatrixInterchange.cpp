f5a0f96df289a25e9c8e0d174e1b79c2d78b3e67
Ubuntu <ubuntu@ip-172-31-5-19.ec2.internal>
2019-09-30 19:26:40 +0000

    commit

---
+++ Arrays/MatrixInterchange.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void intechange(int a[][1000], int r, int c) {
+    int colBeg = 0, colEnd = c-1;
+    for(int i=0; i<r; i++) {
+        int temp = a[i][colBeg];
+        a[i][colBeg] = a[i][colEnd];
+        a[i][colEnd] = temp;
+    }
+    for(int i = 0;i<r;i++){
+        for(int j = 0;j<c;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
